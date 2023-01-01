762e92a4c9cb0c5a4ee775886c00438a4f881486
steve passarelli <passarellism@gmail.com>
2023-01-01 08:28:03 -0700

    commit tree b2372dcd620df1b5f0f2b1570b1f4f123720a617

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+let node = document.getElementById("main");
+if (node.parentNode) {
+  node.parentNode.removeChild(node);
+}
+
+const main = document.querySelector('#main')
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Steve is the champion"
+main.appendChild(newHeader)
+console.log(newHeader)
