c5ff7716ead90f59026c545d2a8fccefb03cbc81
shelby <shelbynarnold@yahoo.com>
2023-01-11 16:01:22 -0700

    solved first half of lab

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function hello = shelby{
-    
+let node = document.getElementById('main')
+if (node.parentNode){
+    node.parentNode.removeChild(node)
 }
+
+const newHeader = document.createElement('h1')
